0515855ef76c1ab3e6aaf89699539e2b91bee616
raphcod <abdulraufuabiola2019@gmail.com>
2023-07-17 09:38:48 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,34 +3,11 @@
 /**
 *main - this is the main function
 *Return: always 0
-*printing first line
-*printing first line
-*printing first line
-*printing first line
-*printing first line
-*printing first line
-*printing first line
-*printing first line
-*printing first line
-*printing first line
-*printing first line
-*printing first line
 */
 
 int main(void)
 {
-	printf("           @\n");
-	printf("          @@@\n");
-	printf("         @@@@@\n");
-	printf("        @@@@@@@\n");
-	printf("       @@@@@@@@@\n");
-	printf("     @@@@@@@@@@@\n");
-	printf("@@@@@@@@@@@\n");
-	printf(" @@@@@@@@@\n");
-	printf("  @@@@@@@\n");
-	printf("   @@@@@\n");
-	printf("    @@@\n");
-	printf("     @\n");
+	printf("with proper grammar, but the outcome is a piece of art\n");
 
 	return (0);
 }
