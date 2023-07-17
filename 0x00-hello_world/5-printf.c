a916e60e0276e014c95c493c8020f49a4a4c1b75
raphcod <abdulraufuabiola2019@gmail.com>
2023-07-17 08:01:32 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+*main - this is the main function
+*Return: always 0
+*printing first line
+*printing first line
+*printing first line
+*printing first line
+*printing first line
+*printing first line
+*printing first line
+*printing first line
+*printing first line
+*printing first line
+*printing first line
+*printing first line
+*/
+
+int main(void)
+{
+	printf("           @\n");
+	printf("          @@@\n");
+	printf("         @@@@@\n");
+	printf("        @@@@@@@\n");
+	printf("       @@@@@@@@@\n");
+	printf("     @@@@@@@@@@@\n");
+	printf("@@@@@@@@@@@\n");
+	printf(" @@@@@@@@@\n");
+	printf("  @@@@@@@\n");
+	printf("   @@@@@\n");
+	printf("    @@@\n");
+	printf("     @\n");
+
+	return (0);
+}
